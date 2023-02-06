41ff1515a2a8744e1cacbf1cab945c1cfcc74eec
marionnevq <quintanamarionne@gmail.com>
2023-02-06 22:41:15 +0800

    added solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,99 @@
 //Call all functions inside services and log updated value/s
-import { updateUser, getAllUsers } from "./services/users";
+import {
+  updateUser,
+  getAllUsers,
+  addUser,
+  getUserById,
+} from "./services/users";
 
-console.log(getAllUsers());
+import {
+  getCommentById,
+  getCommentsByPostId,
+  updateCommentBody,
+  deleteCommentById,
+  addComment,
+} from "./services/comments";
+
+import {
+  getPosts,
+  getPostById,
+  getPostsByUser,
+  addPost,
+  updatePostTitle,
+  updatePostBody,
+  updatePost,
+  deletePostById,
+  deletePostsByUserId,
+} from "./services/posts";
+
+//user tests
+console.log("user tests");
+console.log("getAllUsers", getAllUsers());
+console.log("getUserById (user 7)", getUserById(7));
 updateUser(1, { email: "roger@77.com", website: "hilde-edit.org" });
-console.log(getAllUsers());
+console.log("updateUser (user 1)", getAllUsers());
+addUser({
+  name: "Ciryl Gane",
+  username: "BonGamin",
+  email: "cirylgane@gmail.com",
+  address: {
+    street: "Session Rd.",
+    suite: "Rm. 47",
+    city: "Baguip",
+    zipcode: "12345-6789",
+    geo: {
+      lat: "-30.2222",
+      lng: "82.1111",
+    },
+  },
+  phone: "1-234-567-8900 ",
+  website: "bongamin.edu.org",
+  company: {
+    name: "Universal-Wrestling",
+    catchPhrase: "New face of sports entertainment",
+    bs: "classic and fresh talents",
+  },
+});
+console.log("addUser", getAllUsers());
+
+//comments test
+console.log("comments test");
+
+console.log("getCommentById (comment 1)", getCommentById(1));
+console.log("getCommentsByPostId", getCommentsByPostId(1));
+updateCommentBody(1, "new comment burger fries hotdog lasagna");
+console.log("updateCommentBody (comment 1)", getCommentById(1));
+addComment({
+  postId: 1,
+  name: "New Addition",
+  email: "test@email.biz",
+  body: "this is a test comment",
+}); // generates comment with id 500001.
+console.log("addComment", getCommentById(500001));
+deleteCommentById(500001);
+console.log("deleteComment", getCommentById(500001));
+
+// posts test
+console.log("comments test");
+console.log("getPosts", getPosts());
+console.log("getPostById", getPostById(1));
+console.log("getPostByUserId", getPostsByUser(1));
+addPost({
+  userId: 1,
+  title: "test post",
+  body: "this is a test post",
+}); // creates post with id 100001
+console.log("addPost", getPosts());
+updatePostTitle(100001, "new title");
+console.log("updatePostTitle", getPosts());
+updatePostBody(100001, "new body");
+console.log("updatePostBody", getPosts());
+updatePost(100001, {
+  title: "newest title",
+  body: "newest body",
+});
+console.log("updatePost", getPosts());
+deletePostById(100001);
+console.log("deletePostById", getPostById(100001));
+deletePostsByUserId(1);
+console.log("deletePostsByUserId", getPosts());
--- src/services/posts.js
+++ src/services/posts.js
@@ -1,41 +1,79 @@
 import { posts as data } from "../data/posts";
+import { generateId } from "../utils";
 
 //private
 let posts = [...data];
 
-function getPosts() {
+export function getPosts() {
   //get all posts
+  return [...posts];
 }
 
-function getPostsByUser(userId) {
+export function getPostsByUser(userId) {
   //get all posts by userId
+  return posts.filter((post) => post.userId === userId);
 }
 
-function getPostById(id) {
+export function getPostById(id) {
   //get a single post by id
+  return posts.find((post) => post.id === id);
 }
 
-function addPost(post) {
+export function addPost(post) {
   //add new post to BEGINNING of posts array
   // use generateId function and pass posts array as the argument to generate a unique id.
+  let newPost = { id: generateId(posts), ...post };
+  posts = [...posts, newPost];
 }
 
-function updatePostTitle(id, title) {
+export function updatePostTitle(id, title) {
   //update post title
+  posts = posts.map((post) => {
+    if (post.id === id) {
+      return {
+        ...post,
+        title: title,
+      };
+    }
+
+    return post;
+  });
 }
 
-function updatePostBody(id, body) {
+export function updatePostBody(id, body) {
   //update post body
+  posts = posts.map((post) => {
+    if (post.id === id) {
+      return {
+        ...post,
+        body: body,
+      };
+    }
+
+    return post;
+  });
 }
 
-function updatePost(id, post) {
+export function updatePost(id, post) {
   //update post title and or body (imagine a form where user is allowed to edit both title and post but may also choose to only edit one of them)
+  posts = posts.map((pst) => {
+    if (pst.id === id) {
+      return {
+        ...pst,
+        ...post,
+      };
+    }
+
+    return pst;
+  });
 }
 
-function deletePostBy(id) {
+export function deletePostById(id) {
   //delete post by id
+  posts = posts.filter((post) => post.id !== id);
 }
 
-function deletePostsByUserId(userId) {
+export function deletePostsByUserId(userId) {
   //delete all posts of a user by given userId
+  posts = posts.filter((post) => post.userId !== userId);
 }
